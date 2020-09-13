fdafa2bb3ed2ff5ccb00e7b793097703372a893e
morganfula <morgan.fula@me.com>
2020-09-13 23:31:00 +0200

    v2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 window.addEventListener('mousemove', onMouseMove);
 
-const benjaminCode = document.querySelector('.benjamincode')
+const morganFula = document.querySelector('.morganfula')
 const backgroundSphere = document.querySelector('.background-sphere')
 const foregroundSphere = document.querySelector('.foreground-sphere')
 
@@ -9,6 +9,6 @@ function onMouseMove(e) {
   console.log(movement)
 
   backgroundSphere.style.transform = `translate3d(calc(${movement} * 1vh), 0, 0)`;
-  benjaminCode.style.transform = `translate3d(calc(${-movement} * 2vh), 0, 0)`;
+  morganFula.style.transform = `translate3d(calc(${-movement} * 2vh), 0, 0)`;
   foregroundSphere.style.transform = `translate3d(calc(${-movement} * 8vh), 0, 0)`;
 }
